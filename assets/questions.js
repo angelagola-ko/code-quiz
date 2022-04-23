486eaf455317e923a948a32b7a98464b785040ef
Angela Gola <95582357+angelaa1794@users.noreply.github.com>
2022-04-22 22:03:00 -0400

    Starter Code

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        Q: 'Question 1',
+        A: ['ans5:a','ans1:b','ans1:c','ans1:d'],
+        C: 'ans1:d'
+    },
+    {
+        Q: 'Question 2',
+        A: ['ans2:a','ans2:b','ans2:c','ans2:d'],
+        C: 'ans2:d'
+    },
+    {
+        Q: 'Question 3',
+        A: ['ans3:a','ans3:b','ans3:c','ans3:d'],
+        C: 'ans3:d'
+    },
+    {
+        Q: 'Question 4',
+        A: ['ans4:a','ans4:b','ans4:c','ans4:d'],
+        C: 'ans4:d'
+    },
+    {
+        Q: 'Question 5',
+        A: ['ans5:a','ans5:b','ans5:c','ans5:d'],
+        C: 'ans5:d'
+    }
+]
---
+++ assets/script.js
@@ -0,0 +1,75 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+//  1 Quiz Questions
+//  a. Create a series of questions.
+//  b. Make the answers clickable
+//     ba. Mark Correct/Wrong
+//  d. present another question.
+//     da. Mark Correct/Wrong
+//
+//  2 Timer
+//  a. Start timer when Start button is clicked.
+//   aa. Also, present a question.
+//  b. When question is wrong -> time subtracted from time clock.
+//  c. Time ends -> Game over. 
+//    ab. Create if statement for 2 seconds, 1 second, 0 seconds.
+//
+//  3 Stored Data
+//  a. Store highscores in browser.
+//  b. Grab highscores and present to people.
+
+let qI = 0;
+let clockId;
+let time = 60;
+let banner = document.querySelector('.banner');
+document.querySelector('#start').addEventListener('click',handleStart);
+
+function handleStart() {
+    clockId = setInterval(handleClock,1000);
+    handleQuestions();
+};
+
+function handleQuestions() {
+    if(qI<questions.length){
+        const { Q,A,C } = questions[qI];
+        qI++; 
+        banner.innerHTML = `<h2>${Q}</h2>`;
+        A.forEach(ans => {
+            let btn = document.createElement('button');
+            btn.innerText = ans;
+            btn.addEventListener('click', (e) => {
+                if(C == e.target.innerText){
+                    time += 5;
+                } else {
+                    time -= 5;
+                };
+                handleQuestions();
+            })
+            banner.appendChild(btn);
+        });
+    }
+}
+
+function handleClock() {
+    time--;
+
+    if(time < 1) {
+        clearInterval(clockId);
+        time = 0;
+    };
+
+    document.querySelector('#time').innerHTML = time;
+};
